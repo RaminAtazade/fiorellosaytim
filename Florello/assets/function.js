8599dc8b2019c752b5a94e302211353594a30a19
Ramin Atazade <127185803+RaminAtazade@users.noreply.github.com>
2023-05-11 17:34:28 -0300

    Add files via upload

---
+++ Florello/assets/function.js
@@ -0,0 +1,74 @@
+function myFunction() {
+    var elements = document.querySelectorAll(".foto");
+    elements.forEach(function(element) {
+      if (element.id !== "cactus") {
+        element.style.display = "none";
+      } else {
+        element.style.display = "block";
+      }
+    });
+  }
+//   ilk ve ikinci kodda cuzi fer1 var
+  function myFunction2() {
+    var elements = document.querySelectorAll(".foto");
+    elements.forEach(function(element) {
+        if (element.id == "balcony") {
+          element.style.display = "block";
+    } else {
+        element.style.display = "none";
+      }
+    });
+  }
+
+  function myFunction3() {
+    var elements = document.querySelectorAll(".foto");
+    elements.forEach(function(element) {
+        if (element.id == "exotic") {
+          element.style.display = "block";
+    } else {
+        element.style.display = "none";
+      }
+    });
+  }
+
+  function myFunction4() {
+    var elements = document.querySelectorAll(".foto");
+    elements.forEach(function(element) {
+        if (element.id == "flowers") {
+          element.style.display = "block";
+    } else {
+        element.style.display = "none";
+      }
+    });
+  }
+
+  function myFunction5() {
+    var elements = document.querySelectorAll(".foto");
+    elements.forEach(function(element) {
+        if (element.id == "winter") {
+          element.style.display = "block";
+    } else {
+        element.style.display = "none";
+      }
+    });
+  }
+  function myFunction6() {
+    var elements = document.querySelectorAll(".foto");
+    elements.forEach(function(element) {
+        if (element.id == "popular") {
+          element.style.display = "block";
+    } else {
+        element.style.display = "none";
+      }
+    });
+  }
+  function myFunction7() {
+    var elements = document.querySelectorAll(".foto");
+    elements.forEach(function(element) {
+        if (element.id == "beleidyoxduhehehe") {
+          element.style.display = "none";
+    } else {
+        element.style.display = "block";
+      }
+    });
+  }
